d39b2fc9cfe0a655b30b5809296ecb8e924daabc
BryanBo-Cao <bryan.bo.cao@outlook.com>
2019-02-18 21:48:28 -0500

    Write tree into json file

--- labs/lab01/src/my_code/BryanBoCao_112130213_lab01_CSE564/compute_min_spanning_tree.py
+++ labs/lab01/src/my_code/BryanBoCao_112130213_lab01_CSE564/compute_min_spanning_tree.py
@@ -4,6 +4,7 @@ from scipy.spatial.distance import pdist, squareform
 from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import minimum_spanning_tree
 import csv
+import json
 
 num_data_points = 50
 print("num_data_points: %d" % num_data_points)
@@ -21,12 +22,31 @@ print("minimun number of edges: %d" % np.count_nonzero(mst))
 collage_name = data.iloc[:num_data_points,0:1]
 collage_name_mtx = pd.DataFrame.as_matrix(collage_name)
 
-cse_file_path = "./data/minimum_spanning_tree_mtx.csv"
-with open(cse_file_path,"w+") as csv_file:
-    csvWriter = csv.writer(csv_file,delimiter=',')
-    csvWriter.writerows(mst)
+write_data = {}
+# construct nodes
+write_data['nodes'] = []
+for i in range(num_data_points):
+    node = {}
+    node['id'] = collage_name_mtx[i][0]
+    write_data['nodes'].append(node)
 
-print("Finished writing minimum spanning tree matrix to %s." % cse_file_path)
+# construct links
+write_data['links'] = []
+for i in range(num_data_points):
+    for j in range(num_data_points):
+        if mst[i][j] > 0:
+            link = {}
+            link['source'] = collage_name_mtx[i][0]
+            link['target'] = collage_name_mtx[j][0]
+            link['value'] = str(mst[i][j])
+            write_data['links'].append(link)
+
+print(write_data)
+
+json_file_path = "./data/minimum_spanning_tree_mtx.json"
+with open(json_file_path,"w+") as json_file:
+    json.dump(write_data, json_file)
+print("Finished writing minimum spanning tree matrix to %s." % json_file_path)
 
 '''
 Reference:
